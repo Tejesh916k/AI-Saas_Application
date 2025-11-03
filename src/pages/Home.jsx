5772119b1a446ff594277ec6fcc1f1566b54bd0c
Tejesh916k <tejeshkesanakurthy@gmail.com>
2025-11-03 23:17:55 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Hero from '../components/Hero.jsx'
+import AiTools from '../components/AiTools.jsx'
+import Testimonial from '../components/testimonial.jsx'
+import Plan from '../components/Plan.jsx'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <AiTools/>
+    <Testimonial/>
+    <Plan/>
+    </>
+  )
+}
+
+export default Home
