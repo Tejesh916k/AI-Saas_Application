d804eba8945bcf3176e4ded8143f3fe644b2e10b
Tejesh916k <tejeshkesanakurthy@gmail.com>
2025-11-06 20:03:07 +0530

    added free plan block in dashboard

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -18,7 +18,7 @@ const Layout = () => {
     <div className='flex flex-col items-start justify-start h-screen'>
         <nav className='w-full px-8 min-h-14 flex items-center justify-between
         border-b border-gray-200' >
-          <img src={assets.logo} alt='logo' onClick={()=> Navigate('/')} />
+          <img className='cursor-pointer' src={assets.logo} alt='logo' onClick={()=> Navigate('/')} />
 
         {
           sidebar ? <X onClick={()=> setSidebar(false)} className="w-6 h-6 text-gray-600 sm:hidden" />: 
